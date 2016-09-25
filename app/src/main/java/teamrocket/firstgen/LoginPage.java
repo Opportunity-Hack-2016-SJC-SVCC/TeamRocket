bfc975ad3968184d7e362d61aa968128bc21bedd
CoderRed99 <diannaferris1@gmail.com>
2016-09-25 14:11:56 -0700

    dianna1

--- app/src/main/java/teamrocket/firstgen/LoginPage.java
+++ app/src/main/java/teamrocket/firstgen/LoginPage.java
@@ -1,13 +1,31 @@
 package teamrocket.firstgen;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.*;
+import android.os.*;
+import android.support.v7.app.*;
+import android.view.*;
+import android.widget.*;
 
-public class LoginPage extends AppCompatActivity {
-
+public class LoginPage extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.LoginPage);
+
+        // get the Intent that started this Activity
+        Intent in = getIntent();
+
+        // get the Bundle that stores the data of this Activity
+        Bundle b = in.getExtras();
+
+        // getting data from bundle
+        String login = b.getString("login");
+        EditText text = (EditText) findViewById(R.id.confirmation);
+        if (login == null)
+        {
+            text.setVisibility(View.GONE);
+        }
     }
 }
